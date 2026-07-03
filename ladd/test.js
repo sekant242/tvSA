bad99829a593503dd1d36ed4c3b7230b89b7b99f
sekant242 <sekant242@gmail.com>
2026-07-04 01:07:27 +0300

    Update test.js

--- ladd/test.js
+++ ladd/test.js
@@ -1,21 +1,15 @@
 (function () {
     'use strict';
-
-    // Ждём полной готовности приложения
     Lampa.Listener.follow('app', function (e) {
         if (e.type === 'ready') {
-
-            // Добавляем новый пункт в боковое меню
             Lampa.Menu.addButton({
-                title: 'Тест',      // Название пункта
-                icon: '<svg>...</svg>',  // Иконка (опционально, в формате SVG)
+                title: 'Тест',
+                icon: '...',
                 action: function () {
-                    // Действие при нажатии
                     Lampa.Noty.show('Привет из моего плагина!');
                     console.log('Пункт меню нажат');
                 }
             });
-
             console.log('Плагин успешно загружен!');
         }
     });
